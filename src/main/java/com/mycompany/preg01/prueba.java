0b9b5517b544c220c4e808357bdc01347858e8a2
Quichiz <Quichiz@DESKTOP-7IDUJAR>
2025-05-26 18:44:35 -0500

    Prueba

---
+++ src/main/java/com/mycompany/preg01/prueba.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.preg01;
+
+/**
+ *
+ * @author Quichiz
+ */
+public class prueba {
+    
+}
